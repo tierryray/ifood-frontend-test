db8978da5fc67e1365ec22cda4f36d84d762553f
Tierry Ray <tierry.ray@gmail.com>
2020-08-11 22:10:42 -0300

    feat: initial structure project

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+// import { Container } from './styles';
+
+function Header() {
+  return (
+      <Typography variant="h2">
+          SpotiFood
+      </Typography>
+  )
+}
+
+export default Header;
