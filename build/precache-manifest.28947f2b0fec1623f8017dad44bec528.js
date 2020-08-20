08fff7e6fb2575bcec16aa96b3af7d3acfa3ad3b
Tierry Ray <tierry.ray@gmail.com>
2020-08-19 23:44:25 -0300

    refactor: using ThemeProvider from styled components

---
+++ build/precache-manifest.28947f2b0fec1623f8017dad44bec528.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa63ce2eb064c0432380752dc1100fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40333d5b22608113140a",
+    "url": "/static/css/2.d195aa5e.chunk.css"
+  },
+  {
+    "revision": "049f89955f7fb991af1c",
+    "url": "/static/css/main.091a85de.chunk.css"
+  },
+  {
+    "revision": "40333d5b22608113140a",
+    "url": "/static/js/2.c435957f.chunk.js"
+  },
+  {
+    "revision": "07f815c16e8dd2d3f13e6d9bfc50c371",
+    "url": "/static/js/2.c435957f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "049f89955f7fb991af1c",
+    "url": "/static/js/main.5ee9dd0f.chunk.js"
+  },
+  {
+    "revision": "032a52e1e4336f9d6209",
+    "url": "/static/js/runtime-main.2f61de3a.js"
+  },
+  {
+    "revision": "f6c764a514505110a86de92942995198",
+    "url": "/static/media/ifood-red.f6c764a5.png"
+  }
+]);
